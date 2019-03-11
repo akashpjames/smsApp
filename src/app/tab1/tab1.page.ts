fa2e727b354f232f004b0bc9dfc4772621c1a215
Akash James <akashpjames@gmail.com>
2019-03-11 22:19:02 +0530

    Adding SMS send feature

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,50 @@
 import { Component } from '@angular/core';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+
+declare var SMS: any;
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+
+export class Tab1Page {
+
+    constructor( private androidPermissions: AndroidPermissions ) {}
+
+    checkPermissionAndSend() {
+
+        this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.SEND_SMS).then(
+            success => {
+                if (!success.hasPermission) {
+                    this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.SEND_SMS).
+                    then((success) => {
+                            this.sendMessage();
+                        },
+                        (err) => {
+                            console.error(err);
+                        });
+                }
+            },
+            err => {
+                this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.SEND_SMS).
+                then((success) => {
+                        this.sendMessage();
+                    },
+                    (err) => {
+                        console.error(err);
+                    });
+            });
+    }
+
+    sendMessage(){
+        if(SMS) {
+            SMS.sendSMS("+918089817002", "Test Message", () => {
+                console.log('Message sent successfully');
+            }, (error) => {
+                console.error(error);
+            });
+        }
+    }
+}
